20e2559c311761463c967bb4fe973efdd190a891
adityajakap <aditya.jakap12@gmail.com>
2025-04-28 09:58:31 +0700

    fix layout bugs

--- app/page.tsx
+++ app/page.tsx
@@ -28,22 +28,6 @@ const Page = () => {
   const [loading, setLoading] = useState(true);
   const [hasMounted, setHasMounted] = useState(false);
 
-  useEffect(() => {
-    const updateScale = () => {
-      const screenWidth = window.innerWidth;
-      const scale = screenWidth / 1920; // Dihitung sesuai lebar layar
-      const container = document.getElementById("scaled-container");
-
-      if (container) {
-        // Hanya melakukan scale pada transformasi
-        container.style.transform = `translateX(-50%) scale(${scale})`;
-      }
-    };
-    updateScale();
-    window.addEventListener("resize", updateScale);
-    return () => window.removeEventListener("resize", updateScale);
-  }, []);
-
   // Set hasMounted true setelah komponen dimount
   useEffect(() => {
     setHasMounted(true);
@@ -77,24 +61,19 @@ const Page = () => {
       </div>
     );
   }
-
   return (
     <div className="flex h-screen w-full items-center justify-center">
       {/* Main container with viewport dimensions */}
       <div className="relative h-screen w-screen overflow-hidden">
         {/* Container with fixed dimensions that holds both background and buildings */}
         <div
-          id="scaled-container"
           className="absolute"
           style={{
-            position: isMobile ? "absolute" : "fixed", // Untuk mobile: absolute, desktop: fixed
-            width: "100%", // Untuk mobile dan desktop: 100% lebar
-            height: "100%", // Untuk mobile dan desktop: 100% tinggi
-            left: isMobile ? "0" : "50%", // Untuk mobile: posisikan dari kiri layar, desktop: posisikan di tengah
-            top: "0", // Untuk mobile dan desktop: mulai dari atas layar
-            transform: isMobile ? "none" : "translateX(-50%)", // Mobile: tanpa transformasi, desktop: terpusat
-            transformOrigin: "top center", // Agar konten tetap terpusat di desktop
-            marginLeft: isMobile ? "0" : "auto", // Mobile: tanpa margin, desktop: margin auto untuk terpusat
+            width: isMobile ? "100%" : "1920px",
+            height: isMobile ? "100%" : "1080px",
+            left: "50%",
+            transform: "translateX(-50%)",
+            top: "0",
           }}
         >
           <Button />
